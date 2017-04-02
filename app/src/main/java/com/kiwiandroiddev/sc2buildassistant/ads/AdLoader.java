6dde757ed541205ca7b8055f55805f334a5f2a66
Matt Clarke <kiwiandroiddev@gmail.com>
2017-04-02 16:10:04 +1200

    Use smart sized ad banner in main activity, refactor some ad loading code

--- app/src/main/java/com/kiwiandroiddev/sc2buildassistant/ads/AdLoader.java
+++ app/src/main/java/com/kiwiandroiddev/sc2buildassistant/ads/AdLoader.java
@@ -1,7 +1,9 @@
 package com.kiwiandroiddev.sc2buildassistant.ads;
 
 import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
+import com.kiwiandroiddev.sc2buildassistant.R;
 
 /**
  * Created by matt on 28/02/17.
@@ -10,6 +12,8 @@ public final class AdLoader {
     private AdLoader() {}
 
     public static void loadAdForRealUsers(AdView adView) {
+        adView.setAdUnitId(adView.getContext().getString(R.string.admob_banner_ad_unit_id));
+        adView.setAdSize(AdSize.SMART_BANNER);
         AdRequest adRequest = new AdRequest.Builder()
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
                 .addTestDevice("947D1DE4DD71747C1BDAB51AB10C6F0A")
