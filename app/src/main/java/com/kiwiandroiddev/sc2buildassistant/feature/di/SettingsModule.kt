5355e27e9d194c35d39d561a8f80f84f121ceb18
Matt Clarke <kiwiandroiddev@gmail.com>
2017-04-23 18:25:27 +1200

    Provide real implementation for load standard builds use case

--- app/src/main/java/com/kiwiandroiddev/sc2buildassistant/feature/di/SettingsModule.kt
+++ app/src/main/java/com/kiwiandroiddev/sc2buildassistant/feature/di/SettingsModule.kt
@@ -1,6 +1,9 @@
 package com.kiwiandroiddev.sc2buildassistant.feature.di
 
+import android.content.Context
+import com.kiwiandroiddev.sc2buildassistant.feature.di.qualifiers.ApplicationContext
 import com.kiwiandroiddev.sc2buildassistant.feature.errorreporter.ErrorReporter
+import com.kiwiandroiddev.sc2buildassistant.feature.settings.data.LoadStandardBuildsIntoDatabaseUseCaseImpl
 import com.kiwiandroiddev.sc2buildassistant.feature.settings.domain.LoadStandardBuildsIntoDatabaseUseCase
 import com.kiwiandroiddev.sc2buildassistant.feature.settings.domain.ResetDatabaseUseCase
 import com.kiwiandroiddev.sc2buildassistant.feature.settings.domain.datainterface.ClearDatabaseAgent
@@ -9,7 +12,6 @@ import com.kiwiandroiddev.sc2buildassistant.feature.settings.presentation.Settin
 import com.kiwiandroiddev.sc2buildassistant.feature.settings.presentation.SettingsPresenter
 import dagger.Module
 import dagger.Provides
-import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import javax.inject.Singleton
@@ -29,7 +31,8 @@ class SettingsModule {
                     resetDatabaseUseCase = resetDatabaseUseCase,
                     navigator = navigator,
                     errorReporter = errorReporter,
-                    viewResultScheduler = AndroidSchedulers.mainThread(), executionScheduler = Schedulers.io())
+                    executionScheduler = Schedulers.io(),
+                    viewResultScheduler = AndroidSchedulers.mainThread())
 
     @Provides
     @Singleton
@@ -39,11 +42,7 @@ class SettingsModule {
 
     @Provides
     @Singleton
-    fun provideLoadStandardBuildsIntoDatabaseUseCase(): LoadStandardBuildsIntoDatabaseUseCase =
-            object : LoadStandardBuildsIntoDatabaseUseCase {
-                override fun loadBuilds(): Observable<Int> =
-                        Observable.just(100)
-
-            }
+    fun provideLoadStandardBuildsIntoDatabaseUseCase(@ApplicationContext appContext: Context): LoadStandardBuildsIntoDatabaseUseCase =
+            LoadStandardBuildsIntoDatabaseUseCaseImpl(appContext)
 
 }
---
+++ app/src/main/java/com/kiwiandroiddev/sc2buildassistant/feature/settings/data/LoadStandardBuildsIntoDatabaseUseCaseImpl.kt
@@ -0,0 +1,16 @@
+package com.kiwiandroiddev.sc2buildassistant.feature.settings.data
+
+import android.content.Context
+import com.kiwiandroiddev.sc2buildassistant.feature.di.qualifiers.ApplicationContext
+import com.kiwiandroiddev.sc2buildassistant.feature.settings.domain.LoadStandardBuildsIntoDatabaseUseCase
+import com.kiwiandroiddev.sc2buildassistant.service.JsonBuildService
+import com.kiwiandroiddev.sc2buildassistant.service.StandardBuildsService
+import io.reactivex.Observable
+
+class LoadStandardBuildsIntoDatabaseUseCaseImpl(@ApplicationContext val appContext: Context) : LoadStandardBuildsIntoDatabaseUseCase {
+
+    override fun loadBuilds(): Observable<Int> =
+            StandardBuildsService.getLoadStandardBuildsIntoDBObservable(appContext, true)
+                    .doOnComplete { JsonBuildService.notifyBuildProviderObservers(appContext) }
+
+}
