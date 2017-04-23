d0f90a039bf604a06cdc5169aaf9135c37d1189d
Matt Clarke <kiwiandroiddev@gmail.com>
2017-04-23 17:17:09 +1200

    Implement success and error views in settings fragment

--- app/src/main/java/com/kiwiandroiddev/sc2buildassistant/feature/settings/presentation/SettingsView.kt
+++ app/src/main/java/com/kiwiandroiddev/sc2buildassistant/feature/settings/presentation/SettingsView.kt
@@ -7,5 +7,5 @@ package com.kiwiandroiddev.sc2buildassistant.feature.settings.presentation
 interface SettingsView {
     fun showResetDatabaseConfirmation()
     fun showResetDatabaseSuccess()
-    fun showResetDatabaseError()
+    fun showResetDatabaseError(detailedError: String)
 }
