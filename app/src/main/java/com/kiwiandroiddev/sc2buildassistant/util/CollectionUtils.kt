6a49172b0439e6a3d098f92957e150b93ba2d012
Matt Clarke <kiwiandroiddev@gmail.com>
2017-07-30 13:46:10 +1200

    Fixed translate bar showing up when it shouldn't

--- app/src/main/java/com/kiwiandroiddev/sc2buildassistant/util/CollectionUtils.kt
+++ app/src/main/java/com/kiwiandroiddev/sc2buildassistant/util/CollectionUtils.kt
@@ -3,4 +3,4 @@ package com.kiwiandroiddev.sc2buildassistant.util
 /**
  * Created by matthome on 29/07/17.
  */
-fun noneAreTrue(vararg flags: Boolean) = !(flags.toSet().any())
+fun noneAreTrue(vararg flags: Boolean) = flags.filter { it == true }.isEmpty()
